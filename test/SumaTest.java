2c739384b4476b34a7615466da9f0ecd07cf0d49
SalimVazquez <181086@ids.upchiapas.edu.mx>
2020-03-20 03:42:10 -0600

    Data static

--- test/SumaTest.java
+++ test/SumaTest.java
@@ -10,35 +10,31 @@ class SumaTest {
     public int b = (int) Math.floor(Math.random()*(1000-0+1)+0);
     public int c = (int) Math.floor(Math.random()*(1000-0+1)+0);
     public int d = (int) Math.floor(Math.random()*(1000-0+1)+0);
-    public int e = (int) Math.floor(Math.random()*(1000-0+1)+0);
-    public int f = (int) Math.floor(Math.random()*(1000-0+1)+0);
-    public int g = (int) Math.floor(Math.random()*(1000-0+1)+0);
-    public int h = (int) Math.floor(Math.random()*(1000-0+1)+0);
-    public int i = (int) Math.floor(Math.random()*(1000-0+1)+0);
-    public int j = (int) Math.floor(Math.random()*(1000-0+1)+0);
+    public int e = 10;
+    public int f = 11;
+    public int g = 80;
+    public int h = 1;
+    public int i = 5;
+    public int j = 10;
     public int respuesta;
-    Scanner sc = new Scanner(System.in);
 
     @Test
     void getResultado1() {
-        System.out.println("Ingrese el resultado de la suma #1\n" + a + "+" + b + "=");
-        respuesta = sc.nextInt();
+        respuesta = (int) Math.floor(Math.random()*(1000-0+1)+0);
         Suma s = new Suma(a,b,c,d,e,f,g,h,i,j);
         assertEquals(respuesta, s.getResultado1());
     }
 
     @Test
     void getResultado2() {
-        System.out.println("Ingrese el resultado de la suma #2\n" + c + "+" + d + "=");
-        respuesta = sc.nextInt();
+        respuesta = (int) Math.floor(Math.random()*(1000-0+1)+0);
         Suma s = new Suma(a,b,c,d,e,f,g,h,i,j);
         assertEquals(respuesta, s.getResultado2());
     }
 
     @Test
     void getResultado3() {
-        System.out.println("Ingrese el resultado de la suma #3\n" + e + "+" + f + "=");
-        respuesta = sc.nextInt();
+        respuesta = 21;
         Suma s = new Suma(a,b,c,d,e,f,g,h,i,j);
         assertEquals(respuesta, s.getResultado3());
     }
@@ -46,16 +42,14 @@ class SumaTest {
 
     @Test
     void getResultado4() {
-        System.out.println("Ingrese el resultado de la suma #4\n" + g + "+" + h + "=");
-        respuesta = sc.nextInt();
+        respuesta = 81;
         Suma s = new Suma(a,b,c,d,e,f,g,h,i,j);
         assertEquals(respuesta, s.getResultado4());
     }
 
     @Test
     void getResultado5() {
-        System.out.println("Ingrese el resultado de la suma #5\n" + i + "+" + j + "=");
-        respuesta = sc.nextInt();
+        respuesta = 15;
         Suma s = new Suma(a,b,c,d,e,f,g,h,i,j);
         assertEquals(respuesta, s.getResultado5());
     }
